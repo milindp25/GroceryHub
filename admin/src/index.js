b63697bb5a142b978ed58465b1aa4741438ade1d
Milind Prabhakat <milindp25@gmail.com>
2022-11-18 13:57:02 -0600

    Admin Panel Commit

--- admin/src/index.js
+++ admin/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
+import {Provider} from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
-  </React.StrictMode>,
+  <Provider store ={store}>
+  <PersistGate loading={null} persistor={persistor}>
+  <App />
+  </PersistGate>
+</Provider>,
   document.getElementById("root")
 );
